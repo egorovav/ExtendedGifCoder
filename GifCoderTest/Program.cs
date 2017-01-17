a340f970c4ea47d3f81746428508691e1524add0
egorov_av <egorov_av@mail.ru>
2017-01-17 14:12:41 +0300

    Create Project

---
+++ GifCoderTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using Ja2DataImage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace GifCoderTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var _gifFileName = "truck_crush.gif";
+
+			var _gifCoder = new GifBitmapCoder();
+			var _input = new FileStream(_gifFileName, FileMode.Open);
+
+			_gifCoder.Load(_input);
+
+			_input.Close();
+
+			for (int i = 0; i < _gifCoder.Frames.Count; i++) 
+			{
+				_gifCoder.Frames[i].Delay = i % 22;
+			}
+
+			var _outputFileName = "truck_crush_slow_mo.gif";
+			var _output = new FileStream(_outputFileName, FileMode.Create);
+
+			_gifCoder.Save(_output);
+
+			_output.Close();
+		}
+	}
+}
